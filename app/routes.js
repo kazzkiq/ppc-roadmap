66c26a4c1d351d6b37b5127d4bff81856895d2c8
Kazzkiq <claudiohw@gmail.com>
2017-12-24 20:02:28 -0200

    Removing unused initial routes

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,5 @@
 import roadtrip from 'roadtrip';
 import IndexHandler from './pages/index/index.handler';
-import UsersHandler from './pages/users/users.handler';
-import UserDetailsHandler from './pages/user-details/user-details.handler';
 
 export default class Routes {
   constructor() {
@@ -11,13 +9,9 @@ export default class Routes {
 
   init() {
     this.index_handler = new IndexHandler();
-    this.users_handler = new UsersHandler();
-    this.user_details_handler = new UserDetailsHandler();
 
     this.router
       .add('/', this.index_handler.route)
-      .add('/users', this.users_handler.route)
-      .add('/users/:id', this.user_details_handler.route)
       .start({
         fallback: '/'
       });
