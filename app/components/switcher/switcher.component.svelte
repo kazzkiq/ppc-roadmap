de63423d1ebb4601a8973ca8cf6a363210aff4d2
Kazzkiq <claudiohw@gmail.com>
2017-12-24 19:48:01 -0200

    Adding switcher component

---
+++ app/components/switcher/switcher.component.svelte
@@ -0,0 +1,26 @@
+<div class="SwitcherComp">
+  {{#each options as option}}
+    <a href="#" class="option {{(option.selected) ? 'active' : ''}}" on:click="handleSwitch(option)">{{option.label}}</a>
+  {{/each}}
+</div>
+
+<script>
+  export default {
+    methods: {
+      handleSwitch(e) {
+        const options = this.get('options');
+        
+        options.forEach((option, i) => {
+          if (option.label === e.label) {
+            options[i].selected = true;
+          } else {
+            options[i].selected = false;
+          }
+        });
+
+        this.set({options: options});
+        this.fire('switched', e);
+      }
+    }
+  }
+</script>
--- app/pages/index/index.page.svelte
+++ app/pages/index/index.page.svelte
@@ -1,24 +1,55 @@
 <Topbar/>
-
 <Heading title="Roadmap" subtitle="Follow our release notes and other important milestones." />
 
-<button on:click="goto('users')">Goto Users</button>
+<div class="container">
+  <div class="label">Show in timeline:</div>
+  <Switcher options="{{switcherOptions}}" on:switched="receive(event)" />
+
+  <hr>
+</div>
+
+
+
+<!-- <button on:click="goto('users')">Goto Users</button> -->
 
 <script>
   import roadtrip from 'roadtrip';
   import Topbar from '../../components/topbar/topbar.component';
   import Heading from '../../components/heading/heading.component';
+  import Switcher from '../../components/switcher/switcher.component';
 
   export default {
+    data() {
+      return {
+        switcherOptions: [
+          {
+            label: 'All',
+            selected: true
+          },
+          {
+            label: 'Core',
+            selected: false
+          },
+          {
+            label: 'Community',
+            selected: false
+          }
+        ]
+      }
+    },
     methods: {
       goto(path) {
         roadtrip.goto(path);
+      },
+      receive(e) {
+        console.log(e);
       }
     },
 
     components: {
       Topbar,
-      Heading
+      Heading,
+      Switcher
     }
   }
 </script>
